e684c6b35cb1d556772bdd21ec5317bb65cf7e31
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-07-10 17:33:49 +0530

    customer-tracker.sql
    customer-tracker.sql file addition

--- SQL Scripts/02-customer-tracker.sql
+++ SQL Scripts/02-customer-tracker.sql
@@ -1,22 +1,19 @@
 CREATE DATABASE  IF NOT EXISTS web_customer_tracker;
 USE web_customer_tracker;
-
 DROP TABLE IF EXISTS customer;
+
 CREATE TABLE customer (
   id int(11) NOT NULL AUTO_INCREMENT,
   first_name varchar(45) DEFAULT NULL,
   last_name varchar(45) DEFAULT NULL,
   email varchar(45) DEFAULT NULL,
   PRIMARY KEY (id)
-);
-
-LOCK TABLES `customer` WRITE;
-
-INSERT INTO `customer` VALUES 
-	(1,'David','Adams','david@website.com'),
-	(2,'John','Doe','john@website.com'),
-	(3,'Ajay','Rao','ajay@website.com'),
-	(4,'Mary','Public','mary@website.com');
-	
+); 
+LOCK TABLES customer WRITE;
+INSERT INTO customer VALUES 
+	(1,'Harshit','Choudhary','david@website.com'),
+	(2,'Ankit','Garg','john@website.com'),
+	(3,'Monica','Sharma','ajay@website.com'),
+	(4,'Bhavya','Shetty','mary@website.com');
 UNLOCK TABLES;
 
